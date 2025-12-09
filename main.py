1983bc51b6a5d4e3ebe538c161bd557a17a91eeb
rbkft-jim <learning@rbkft.eu>
2025-12-09 21:56:11 +0100

    Test du workdlow

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/test")
 def main():
-    return "Hello World de Gfunctions v2"
+    return "Hello World de Gfunctions v1"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True, port=8080)
